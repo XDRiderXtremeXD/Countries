407a292531139fa156d6c3a97c5fa6d552ee48f4
Angelo <rideralucar@gmail.com>
2025-07-12 13:50:30 -0500

    actualizacion

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // 👈 cambia esto
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root')); // 👈 usa createRoot
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://sbit.ly/CRA-vitals
 reportWebVitals();
