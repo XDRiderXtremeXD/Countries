fea6c288935532e7882cb9117013aaf6ee8822bb
Angelo <rideralucar@gmail.com>
2025-07-12 14:04:55 -0500

    se elimino los console debug

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,8 +2,6 @@
 const API_URL = process.env.REACT_APP_API_URL;
 
 export function getcountries(nombrePais,pagina){
-    console.log("URL ",API_URL);
-    console.log("Prueba");
     return function (dispatch){
       let getData=nombrePais?`${API_URL}/countries/?name=${nombrePais}`:
       `${API_URL}/countries/`
@@ -65,43 +63,3 @@ export function postData(data){
 
   return;
 }
-
-/*
-        return axios.get(`http://www.omdbapi.com/?apikey=1bc6c554&s=${titulo}`)
-          .then(res => res.data)
-          .then(res =>
-            {
-          dispatch({type: "GET_MOVIES", payload: res})
-      })
-
-*/
-/*
-export function addMovieFavorite (payload) {
-return{
-type: "ADD_MOVIE_FAVORITE",
-payload: payload
-}
-}
-
-export function removeMovieFavorite(id){
-return{
-    type: "REMOVE_FAVORITE",
-    payload: id
-}
-}
-
-export function getMovieDetail(id){
-    return function (dispatch){
-        return fetch (`http://www.omdbapi.com/?apikey=1bc6c554&i=${id}`)
-        .then(res => res.json())
-        .then(res => {
-            dispatch({type: "GET_DETAIL", payload: res})
-        })
-    }
-}
-
-export function clearDetail() {
-    return{
-        type: "CLEAR_DETAIL"
-    }
-}*/
